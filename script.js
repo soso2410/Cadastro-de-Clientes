de3bb8c6009af402c4a8374f5fc1526a8123286c
soso2410 <sophiavfa@gmail.com>
2025-11-04 12:59:31 -0300

    closes #6 - JS

--- script.js
+++ script.js
@@ -1,51 +1,73 @@
 $(document).ready(function() {
-    
+    // Aplica a máscara no campo CEP
     $('#cep').mask('00000-000');
 
-    
+
     $('#cep').on('blur', function() {
         var cep = $(this).val().replace(/\D/g, ''); 
 
         
         if (cep.length === 8) {
-            
             $.ajax({
                 url: 'https://viacep.com.br/ws/' + cep + '/json/',
                 type: 'GET',
                 dataType: 'json',
                 success: function(data) {
-                    
                     if (!data.erro) {
-                        
-                        $('#endereco').val(data.logradouro || ''); 
+                        $('#endereco').val(data.logradouro || '');
                         $('#bairro').val(data.bairro || '');
-                        $('#cidade').val(data.localidade || ''); 
-                        $('#estado').val(data.uf || ''); 
-                    } else {
-                        alert('CEP não encontrado. Verifique o valor digitado.');
-                       
-                        $('#endereco').val('');
-                        $('#bairro').val('');
-                        $('#cidade').val('');
-                        $('#estado').val('');
+                        $('#cidade').val(data.localidade || '');
+                        $('#estado').val(data.uf || '');
                     }
-                },
-                error: function() {
-                    alert('Erro ao buscar informações do CEP. Tente novamente.');
-                    
-                    $('#endereco').val('');
-                    $('#bairro').val('');
-                    $('#cidade').val('');
-                    $('#estado').val('');
                 }
             });
-        } else if (cep.length > 0) {
-            
-            alert('CEP inválido. Digite um CEP com 8 dígitos.');
         }
     });
-});
 
+    
+    var contador = 1;
+
+    
+    $('form').on('submit', function(e) {
+        e.preventDefault(); 
+
+        
+        var nome = $('#nome').val();
+        var sobrenome = $('#sobrenome').val();
+        var nomeCompleto = nome + ' ' + sobrenome;
+        var endereco = $('#endereco').val();
+        var numero = $('#numero').val();
+        var enderecoCompleto = endereco + (numero ? ', ' + numero : ''); // Junta endereço e número, se houver
+        var cep = $('#cep').val();
+        var bairro = $('#bairro').val();
+        var cidade = $('#cidade').val();
+        var estado = $('#estado').val();
+
+        
+        var table = $('table');
+        var tbody = table.find('tbody');
+        if (tbody.length === 0) {
+            tbody = $('<tbody></tbody>');
+            table.append(tbody);
+        }
+
+        
+        var row = '<tr>' +
+            '<td>' + contador + '</td>' +
+            '<td>' + nomeCompleto + '</td>' +
+            '<td>' + enderecoCompleto + '</td>' +
+            '<td>' + cep + '</td>' +
+            '<td>' + bairro + '</td>' +
+            '<td>' + cidade + '</td>' +
+            '<td>' + estado + '</td>' +
+            '</tr>';
+
+        
+        tbody.append(row);
+        contador++;
+        $('form')[0].reset();
+    });
+});
         
 
     
